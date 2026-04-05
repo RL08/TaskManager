a80da8d93c85a75327d16a12f5ed482a9710cb33
RL08 <dev.rl08@outlook.com>
2026-04-05 20:47:57 +0200

    refactor: change single quote to double

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { cn } from "@/src/lib/utils";
 import { Roboto } from "next/font/google";
-const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({
   children,
